d8b233a35ce8a16a240aeb7b2c3e36b51d2a3042
wuyinjie <1518986465@qq.com>
2015-03-04 23:59:34 +0800

    添加Template管理类

---
+++ coc/Classes/TemplatesManager.cpp
@@ -0,0 +1,72 @@
+#include "Base.h"
+#include "TemplatesManager.h"
+#include "TabFileReader.h"
+
+static TemplateManager* s_templateManager = nullptr;
+
+TemplateManager::~TemplateManager()
+{
+    for (auto& iter : _npcTemplatesMap)
+    {
+        if (iter.second)
+        {
+            CC_SAFE_DELETE(iter.second);
+        }
+    }
+    _npcTemplatesMap.clear();
+}
+
+TemplateManager* TemplateManager::getInstance()
+{
+    if (s_templateManager == nullptr)
+    {
+        s_templateManager = new TemplateManager();
+        s_templateManager->init();
+    }
+
+    return s_templateManager;
+}
+
+NpcTemplate* TemplateManager::getNpcTemplateBy(const string& jobName)
+{
+    NpcTemplate* npcTemplate = nullptr;
+
+    auto iter = _npcTemplatesMap.find(jobName);
+    if (iter != _npcTemplatesMap.end())
+    {
+        npcTemplate = iter->second;
+    }
+
+    return npcTemplate;
+}
+
+bool TemplateManager::init()
+{
+    bool result = false;
+
+    TabFileReader tabFileReader;
+    if (tabFileReader.open("NpcTemplates.tab"))
+    {
+        for (int i = 0; i < tabFileReader.getRowCount(); i ++)
+        {
+            string jobName = tabFileReader.getString(i, "NpcJobName");
+
+            NpcTemplate* npcTemplate = new NpcTemplate();
+            npcTemplate->moveToNorthEastAnimationPList = tabFileReader.getString(i, "MoveToNorthEastAnimationPList");
+            npcTemplate->moveToEastAnimationPList = tabFileReader.getString(i, "MoveToEastAnimationPList");
+            npcTemplate->moveToSouthEastAnimationPList = tabFileReader.getString(i, "MoveToSouthEastAnimationPList");
+            npcTemplate->moveToSouthWestAnimationPList = tabFileReader.getString(i, "MoveToSouthWestAnimationPList");
+            npcTemplate->moveToWestAnimationPList = tabFileReader.getString(i, "MoveToWestAnimationPList");
+            npcTemplate->moveToNorthWestAnimationPList = tabFileReader.getString(i, "MoveToNorthWestAnimationPList");
+
+            npcTemplate->dieAnimationPList = tabFileReader.getString(i, "DieAnimationPList");
+            npcTemplate->standAnimationPList = tabFileReader.getString(i, "StandAnimationPList");
+
+            _npcTemplatesMap[jobName] = npcTemplate;
+        }
+
+        result = true;
+    }
+
+    return result;
+}
---
+++ coc/Classes/TemplatesManager.h
@@ -0,0 +1,30 @@
+#pragma once
+
+struct NpcTemplate 
+{
+    string moveToNorthEastAnimationPList;
+    string moveToEastAnimationPList;
+    string moveToSouthEastAnimationPList;
+    string moveToSouthWestAnimationPList;
+    string moveToWestAnimationPList;
+    string moveToNorthWestAnimationPList;
+    string dieAnimationPList;
+    string standAnimationPList;
+};
+
+class TemplateManager
+{
+public:
+    ~TemplateManager();
+
+    static TemplateManager* getInstance();
+    NpcTemplate* getNpcTemplateBy(const string& jobName);
+private:
+    bool init();
+
+    map<string, NpcTemplate*> _npcTemplatesMap;
+
+    TemplateManager(){}
+    TemplateManager(const TemplateManager&);
+    TemplateManager& operator = (const TemplateManager&);
+};
