fc066c86eae739029bd148cb7551a75412f50279
wuyinjie <1518986465@qq.com>
2015-03-04 09:23:47 +0800

    添加创建GameObject的函数

--- coc/Classes/GameObject.cpp
+++ coc/Classes/GameObject.cpp
@@ -5,3 +5,20 @@ GameObject::~GameObject()
 {
 
 }
+
+GameObject* GameObject::create(GameObjectType type, const string& jobName)
+{
+    GameObject* gameObject = nullptr;
+
+    switch (type)
+    {
+    case GameObjectType::Npc:
+        break;
+    case GameObjectType::Building:
+        break;
+    default:
+        break;
+    }
+
+    return gameObject;
+}
--- coc/Classes/GameObject.h
+++ coc/Classes/GameObject.h
@@ -12,7 +12,10 @@ class GameObject : public Sprite
 {
 public:
     virtual ~GameObject();
+
+    static GameObject* create(GameObjectType type, const string& jobName);
 protected:
     int _hp = 0;
+    int _maxHp = 0;
     GameObjectType _type = GameObjectType::Invalid;
 };
