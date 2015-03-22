b6168ca6e4ca11117b3318cba01e904608119a47
wuyinjie <1518986465@qq.com>
2015-03-22 23:47:46 +0800

    添加用于创建GameObject施放子弹到场景中的Manager

---
+++ coc/Classes/BulletManager.cpp
@@ -0,0 +1,74 @@
+#include "Base.h"
+#include "GameObject.h"
+#include "BulletManager.h"
+#include "TemplatesManager.h"
+#include "GameObjectManager.h"
+
+static BulletManager* s_bulletManager = nullptr;
+
+const float ARROW_MOVE_SPEED_BY_PIXEL = 1000.0f;
+
+BulletManager* BulletManager::getInstance()
+{
+    if (!s_bulletManager)
+    {
+        s_bulletManager = new BulletManager();
+    }
+
+    return s_bulletManager;
+}
+
+Node* BulletManager::createBullet(BulletType bulletType, int attackerID, int attackTargetID)
+{
+    Node* bullet = nullptr;
+
+    auto attacker = GameObjectManager::getInstance()->getGameObjectBy(attackerID);
+    auto target = GameObjectManager::getInstance()->getGameObjectBy(attackTargetID);
+
+    if (attacker && target)
+    {
+        switch (bulletType)
+        {
+        case BulletType::Arrow:
+            bullet = createArrow(bulletType, attacker, target);
+            break;
+        default:    break;
+        }
+    }
+
+    return bullet;
+}
+
+Node* BulletManager::createArrow(BulletType bulletType, GameObject* attacker, GameObject* target)
+{
+    auto bulletTemplate = TemplateManager::getInstance()->getBulletTemplateBy(bulletType);
+    auto arrow = ParticleSystemQuad::create(bulletTemplate->bulletPList);
+
+    auto attackerPosition = attacker->getPosition();
+    arrow->setPosition(attackerPosition);
+
+    auto targetPosition = target->getPosition();
+    auto distance = sqrt((attackerPosition.x - targetPosition.x) * (attackerPosition.x - targetPosition.x) +
+        (attackerPosition.y - targetPosition.y) * (attackerPosition.y - targetPosition.y));
+
+    auto duration = distance / ARROW_MOVE_SPEED_BY_PIXEL;
+    auto moveTo = MoveTo::create(duration, targetPosition);
+    auto onMoveEnd = CallFunc::create(CC_CALLBACK_0(BulletManager::onBulletMoveEnd, this, arrow, target->getUniqueID(), attacker->getAttackPower()));
+    auto sequenceAction = Sequence::create(moveTo, onMoveEnd, nullptr);
+    arrow->runAction(sequenceAction);
+
+    return arrow;
+}
+
+void BulletManager::onBulletMoveEnd(Node* bullet, int attackTargetID, int damageAmount)
+{
+    auto attackTarget = GameObjectManager::getInstance()->getGameObjectBy(attackTargetID);
+    if (attackTarget)
+    {
+        attackTarget->costHP(damageAmount);
+    }
+
+    bullet->retain();
+    bullet->removeFromParent();
+    bullet->autorelease();
+}
---
+++ coc/Classes/BulletManager.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class GameObject;
+
+class BulletManager
+{
+public:
+    static BulletManager* getInstance();
+    Node* createBullet(BulletType bulletType, int attackerID, int attackTargetID);
+private:
+    Node* createArrow(BulletType bulletType, GameObject* attacker, GameObject* target);
+    void onBulletMoveEnd(Node* bullet, int attackTargetID, int damageAmount);
+
+    BulletManager(){}
+    BulletManager(const BulletManager&);
+    BulletManager& operator = (const BulletManager&);
+};
